e8fbe8a2350dc831144b9aed9320c2c6275c293d
Fernando R P Marques <frpm1001@gmail.com>
2021-03-30 00:08:26 -0300

    Attempt to fix app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "../../assets/styles/application"
 import "channels"
 
 Rails.start()
